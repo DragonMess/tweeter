7d6beb255aa1e4e2cbf6e4163fa44194253e2139
DragonMess <kmylop@hotmail.com>
2020-02-27 02:45:49 +0000

    add style to header-layout-new_tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  const $textarea = $("textarea");
+  const $counter = $(".counter");
+
+  $textarea.on("keyup", () => {
+    let count = $textarea.val().length;
+    $counter.text(count);
+
+    if (count > 140) {
+      $counter.addClass("red");
+    } else if (count < 141) {
+      $counter.removeClass("red");
+    }
+  });
+
+  const $article = $("article");
+
+$article.hover(
+    function() {
+      $(this).addClass("hover");
+    },
+    function() {
+      $(this).removeClass("hover");
+    }
+  );
+
+});
