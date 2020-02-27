f52bee4d09f19f17521e0dfc853e226e31985d7b
DragonMess <kmylop@hotmail.com>
2020-02-27 03:09:57 +0000

    Preventing XSS with Escaping

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,22 +33,25 @@ $(document).ready(function() {
       });
   }
   
-  
+  function dateTime(unix_timestamp) { 
+    let date = new Date(unix_timestamp * 1000);
+    // Hours part from the timestamp
+    let hours = date.getHours();
+    // Minutes part from the timestamp
+    let minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    let seconds = "0" + date.getSeconds();
+    
+    // Will display time in 10:30:23 format
+    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return formattedTime;
+  }
+
    function createTweetElement(tweet){
-      let timestamp = tweet.created_at;
-      
-      let date = new Date(timestamp * 1000);
-      // Hours part from the timestamp
-      let hours = date.getHours();
-      // Minutes part from the timestamp
-      let minutes = "0" + date.getMinutes();
-      // Seconds part from the timestamp
-      let seconds = "0" + date.getSeconds();
-      
-      // Will display time in 10:30:23 format
-      let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
-
+      let safeTxt = $("<p>").text(tweet.content.text).prop('outerHTML');
+      let created = dateTime(tweet.created_at);
+      
       let $tweet = (`<article class="tweet">
       <header>
         <div>
@@ -57,10 +60,10 @@ $(document).ready(function() {
         </div>
         <div>${tweet.user.handle}</div>
       </header>
-      <div class="tweet_text">${tweet.content.text}</div>
+      <div class="tweet_text">${safeTxt}</div>
       <hr>
       <footer>
-        <div>${formattedTime}</div>
+        <div>${created}</div>
         <div class="icons">
           <div class="flag">Flag</div>
           <div class="update">update</div>
@@ -104,7 +107,7 @@ $(document).ready(function() {
     }else {
       $.ajax({ 
         method: 'POST',
-        url:'/tweetsk',
+        url:'/tweets',
         data:$datas
       })
       .done(loadTweets)
@@ -117,13 +120,13 @@ $(document).ready(function() {
   });
   //hide new tweet
   // add class arrow down to the image
-  $("#arrow").slideToggle(4);
+
 
   $("#arrow").on("click", () => {
 
     $(".new-tweet").slideToggle(400);
     $("textarea").focus();
-    // $("#arrow").addClass('arrow-Down');
+    $("#arrow").addClass('arrow-Down');
 
   });
 });
